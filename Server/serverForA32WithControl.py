a6455ee3b41bf3fb95150a988debcc074405c441
Angie Yang <tenglanyang@gmail.com>
2025-09-12 11:23:42 +0100

    change folder name

--- Server/serverForA32WithControl.py
+++ Server/serverForA32WithControl.py
@@ -3,12 +3,15 @@ import threading
 import os
 import sys
 import shutil
-import time
+from datetime import datetime
 
 # Add path to your OWL script
-sys.path.append(rf"C:\Users\88690\Desktop\Dissertation\image processing")
-import test_owlv2_singleImage as owl  # assuming you wrapped OWL detection as a function
-
+sys.path.append(rf"C:\Users\88690\Desktop\Dissertation\image_processing")
+import owlv2_singleImage as owl  # assuming you wrapped OWL detection as a function
+import doclayout_singleImage as dlo
+import filterByVisualCue as fvc
+import tesseractAndGemini as tg
+# import yolo_singleImage as yolo
 # Folder to save incoming images
 save_folder = os.path.join(os.path.dirname(__file__), "imageFromPhone")
 os.makedirs(save_folder, exist_ok=True)
@@ -18,7 +21,7 @@ image_clients = []     # sockets for image streaming
 control_clients = []   # sockets for control + text
 latest_image_path = os.path.join(save_folder, "latest.jpg")
 processing_image_path = os.path.join(save_folder, "processing.jpg")
-processing_lock = threading.Lock()
+#processing_lock = threading.Lock()
 new_image_event = threading.Event()
 pause_event = threading.Event()
 pause_event.set()  # allow processing initially
@@ -100,20 +103,23 @@ def process_images_loop():
         pause_event.wait()
 
         # Copy latest image to processing file
-        with processing_lock:
-            shutil.copy2(latest_image_path, processing_image_path)
+        # with processing_lock: 
+        shutil.copy2(latest_image_path, processing_image_path)
 
         try:
             print("Processing image with OWL...")
-            result = owl.detect_pens(processing_image_path)
-            pen_count = result[0]
+            pen_count = owl.detect_pens(processing_image_path, save_img_path = "owl_result.jpg", save_label_path = "owl_result.txt")
             print(f"Detected {pen_count} pens in the image.")
 
-            if pen_count >= 2:
-                print("✅ At least 2 pens detected, sending PAUSE command.")
+            if pen_count == 2:
+                print("Exactly 2 pens detected, sending PAUSE command.")
                 send_to_control_clients("PAUSE")
                 pause_event.clear()  # pause loop until client sends RESUME
-                send_to_control_clients("answer")
+                resultDoclayout = dlo.predict_document(processing_image_path)
+                output = fvc.process_doclayout_with_pens(img_path = processing_image_path, owl_txt = "owl_result.txt", doclayout_detections = resultDoclayout)
+                answer = tg.process_image_and_query(image_path = processing_image_path, image_json_path = "filtered_text_between_pens.json")
+                print(answer)
+                send_to_control_clients(answer)
 
         except Exception as e:
             print(f"Error during OWL processing: {e}")
@@ -124,6 +130,28 @@ def process_images_loop():
                 print("New image received during processing, continuing loop...")
                 new_image_event.set()
 
+#'''
+if __name__ == "__main__":
+    processing_image_path = rf"C:\Users\88690\Desktop\Dissertation\imagesFromPhone\ground_truth_q02.jpg"
+    ground_truth_path = rf"C:\Users\88690\Desktop\Dissertation\imagesFromPhone\ground_truth_q02.txt"
+    print(f"Processing image with OWL... at {datetime.now().strftime('%H:%M:%S.%f')[:-3]}")
+    pen_count = owl.detect_pens(processing_image_path, save_img_path = "owl_result.jpg", save_label_path = "owl_result.txt")
+    #pen_count = yolo.detect_pens(processing_image_path, save_img_path="yolo_result.jpg", save_label_path="yolo_result.txt")
+    print(f"Detected {pen_count} pens in the image at {datetime.now().strftime('%H:%M:%S.%f')[:-3]}.")
+
+    if pen_count == 2:
+        print("Exactly 2 pens detected, sending PAUSE command.")
+        send_to_control_clients("PAUSE")
+        pause_event.clear()  # pause loop until client sends RESUME
+        print(f"\nProcessing image with Doclayout... at {datetime.now().strftime('%H:%M:%S.%f')[:-3]}")
+        resultDoclayout = dlo.predict_document(processing_image_path)
+        print(f"\nProcessing image with filterByVisualCue... at {datetime.now().strftime('%H:%M:%S.%f')[:-3]}")
+        output = fvc.process_doclayout_with_pens(img_path = processing_image_path, owl_txt = "owl_result.txt", doclayout_detections = resultDoclayout)
+        print(f"\nProcessing image with OCR and LLM... at {datetime.now().strftime('%H:%M:%S.%f')[:-3]}")
+        answer = tg.process_image_and_query(image_path = processing_image_path, image_json_path = "filtered_text_between_pens.json", ground_truth_path=ground_truth_path)
+        print(f"\nThis answer is returned from Gemini: {answer}")
+        send_to_control_clients(answer)
+#'''
 
 # -------------------------
 # Server setup
@@ -169,3 +197,5 @@ while True:
     elif cmd.startswith("TEXT "):
         text = cmd[5:]
         send_to_control_clients(f"TEXT:{text}")
+
+
